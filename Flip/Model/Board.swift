ecd18d6aab04d5ca308cd3cda32b905d681f06ad
Yury Shkoda <shkoda.yuri@gmail.com>
2018-10-29 10:36:48 -0400

    Added Monte Carlo Strategy

--- Flip/Model/Board.swift
+++ Flip/Model/Board.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import GameplayKit
 
-class Board: NSObject {
+class Board: NSObject, GKGameModel {
     static let size = 8
     static let moves = [
         Move(row: -1, col: -1),
@@ -22,6 +23,12 @@ class Board: NSObject {
     
     var rows = [[StoneColor]]()
     var currentPlayer = Player.allPlayers[0]
+    var players: [GKGameModelPlayer]? {
+        return Player.allPlayers
+    }
+    var activePlayer: GKGameModelPlayer? {
+        return currentPlayer
+    }
     
     func canMoveIn(row: Int, col: Int) -> Bool {
         // test 1: move is sensible
@@ -102,5 +109,74 @@ class Board: NSObject {
         }
         
         return didCapture
+    }
+    
+    func getScores() -> (black: Int, white: Int) {
+        var black = 0
+        var white = 0
+        
+        rows.forEach {
+            $0.forEach {
+                if $0 == .black {
+                    black += 1
+                } else if $0 == .white {
+                    white += 1
+                }
+            }
+        }
+        
+        return (black, white)
+    }
+    
+    func isWin(for player: GKGameModelPlayer) -> Bool {
+        guard let playerObject = player as? Player else { return false }
+        
+        let scores = getScores()
+        
+        if playerObject.stoneColor == .black {
+            return scores.black > scores.white + 10
+        } else {
+            return scores.white > scores.black + 10
+        }
+    }
+    
+    func gameModelUpdates(for player: GKGameModelPlayer) -> [GKGameModelUpdate]? {
+        guard let playerObject = player as? Player else { return nil }
+        
+        if isWin(for: playerObject) || isWin(for: playerObject.opponent) { return nil }
+        
+        var moves = [Move]()
+        
+        for row in 0 ..< Board.size {
+            for col in 0 ..< Board.size {
+                if canMoveIn(row: row, col: col) {
+                    moves.append(Move(row: row, col: col))
+                }
+            }
+        }
+        
+        return moves
+    }
+    
+    func apply(_ gameModelUpdate: GKGameModelUpdate) {
+        guard let move = gameModelUpdate as? Move else { return }
+        
+        _ = makeMove(player: currentPlayer, row: move.row, col: move.col)
+        
+        currentPlayer = currentPlayer.opponent
+    }
+    
+    func setGameModel(_ gameModel: GKGameModel) {
+        guard let board = gameModel as? Board else { return }
+        
+        currentPlayer = board.currentPlayer
+        rows = board.rows
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = Board()
+        copy.setGameModel(self)
+        
+        return copy
     }
 }
--- Flip/Model/Player.swift
+++ Flip/Model/Player.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import GameplayKit
 
-class Player: NSObject {
+class Player: NSObject, GKGameModelPlayer {
     static let allPlayers = [Player(stone: .black), Player(stone: .white)]
+    
+    var playerId: Int
     
     var stoneColor: StoneColor
     
     init(stone: StoneColor) {
         stoneColor = stone
+        playerId   = stone.rawValue
     }
     
     var opponent: Player {
